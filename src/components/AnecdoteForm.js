2fdf8df99df7e082db895a479089306675a89f83
Panu Oksiala <panu@oksiala.fi>
2018-12-15 03:18:42 +0200

    6-04

--- src/components/AnecdoteForm.js
+++ src/components/AnecdoteForm.js
@@ -1,27 +1,32 @@
 import React from 'react'
 import { anecdoteCreate } from './../reducers/anecdoteReducer'
+import { notificationClear, notificationSet } from './../reducers/notificationReducer'
 
 class AnecdoteForm extends React.Component {
   handleSubmit = (e) => {
     e.preventDefault()
     const content = e.target.anecdote.value
-    this.props.store.dispatch(
+    const { store } = this.props
+    store.dispatch(
       anecdoteCreate(content)
     )
-  
+    store.dispatch(
+      notificationSet(`You created anecdote ${content}`)
+    )
+    setTimeout(() => store.dispatch(notificationClear()), 5000)
     e.target.anecdote.value = ''
   }
-   render() {
-     return (
-       <div>
-      <h2>create new</h2>
+  render() {
+    return (
+      <div>
+        <h2>create new</h2>
         <form onSubmit={this.handleSubmit}>
           <div><input name='anecdote'/></div>
-          <button>create</button> 
+          <button>create</button>
         </form>
       </div>
-     )
-   }
+    )
+  }
 }
 
 export default AnecdoteForm
--- src/components/AnecdoteList.js
+++ src/components/AnecdoteList.js
@@ -1,7 +1,20 @@
 import React from 'react'
 import { anecdoteVote } from './../reducers/anecdoteReducer'
+import { notificationClear, notificationSet } from './../reducers/notificationReducer'
 
 class AnecdoteList extends React.Component {
+
+  voteAnecdote = ({ content, id }) => () => {
+    const { store } = this.props
+    store.dispatch(
+      anecdoteVote(id)
+    )
+    store.dispatch(
+      notificationSet(`you voted '${content}'`)
+    )
+    setTimeout(() => store.dispatch(notificationClear()), 5000)
+  }
+
   render() {
     const anecdotes = this.props.store.getState().anecdotes
     return (
@@ -14,9 +27,7 @@ class AnecdoteList extends React.Component {
             </div>
             <div>
               has {anecdote.votes}
-              <button onClick={() =>
-                this.props.store.dispatch(anecdoteVote(anecdote.id))
-              }>
+              <button onClick={this.voteAnecdote(anecdote)}>
                 vote
               </button>
             </div>
